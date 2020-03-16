d2cc0af8991dc262422f29b9d115999f093e1444
anandadwi45 <anandadwi45@gmail.com>
2020-03-16 13:56:39 +0800

    Layout History Merchant

---
+++ app/src/main/java/com/tyga/fintech/HistoryMerchantActivity.java
@@ -0,0 +1,14 @@
+package com.tyga.fintech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistoryMerchantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_merchant);
+    }
+}
