69bed5efd48c576ea95d77e4b7333ea4adcd0f56
gedhecamp <gsuarnata99@gmail.com>
2020-03-17 21:49:18 +0800

    create_promo

---
+++ app/src/main/java/com/tyga/fintech/CreatePromoActivity.java
@@ -0,0 +1,13 @@
+package com.tyga.fintech;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreatePromoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_promo);
+    }
+}
